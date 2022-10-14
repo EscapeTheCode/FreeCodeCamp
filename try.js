a3abeef3aaa39cfc40fb4a741e865d6ed3873e20
EscapeTheCode <102279926+EscapeTheCode@users.noreply.github.com>
2022-10-14 14:41:29 +0200

    init

---
+++ try.js
@@ -0,0 +1,7 @@
+const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop();
+console.log(oneDown);
+console.log(threeArr);
+
+/* The first console.log will display the value 6,
+ and the second will display the value [1, 4] */
